c9fa33679093c7d56934af2e31f55844cc8fade3
Serhii Brekhov <bruksbond@gmail.com>
2023-09-28 15:34:07 +0300

    connect database sqlite

---
+++ src/posts/post.entity.ts
@@ -0,0 +1,17 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { PostStatus } from './post-status.enum';
+
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  body: string;
+
+  @Column()
+  status: PostStatus;
+}
