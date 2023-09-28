8bf982b40c8aeb5aaad56bb37bcb0b1a90f8260c
Serhii Brekhov <bruksbond@gmail.com>
2023-09-28 18:16:01 +0300

    add category

---
+++ src/categories/categories.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { CategoriesService } from './categories.service';
+import { Category } from './category.entity';
+
+@Controller('categories')
+export class CategoriesController {
+  constructor(private readonly categoriesService: CategoriesService) {}
+
+  @Post()
+  createCategory(
+    @Body() createCategoryDto: CreateCategoryDto,
+  ): Promise<Category> {
+    return this.categoriesService.createCategory(createCategoryDto);
+  }
+}
